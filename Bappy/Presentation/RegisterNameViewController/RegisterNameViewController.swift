8dcb73f23cf80b9f53f3f5c2ef1fe4164f559149
Dongcheon Jeong <oluye7@naver.com>
2022-05-10 21:43:32 +0900

    Roboto Font 추가 및 UIFont Extension 설정

---
+++ Bappy/Presentation/RegisterNameViewController/RegisterNameViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterNameViewController.swift
+//  Bappy
+//
+//  Created by 정동천 on 2022/05/10.
+//
+
+import UIKit
+import SnapKit
+
+final class RegisterNameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+    }
+}
---
+++ Bappy/Util/Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Extension.swift
+//  Bappy
+//
+//  Created by 정동천 on 2022/05/10.
+//
+
+import UIKit
+
+extension UIFont {
+    enum Family: String {
+        case Black, Bold, Light, Medium, Regular, Thin
+    }
+    
+    static func roboto(size: CGFloat, family: Family = .Regular) -> UIFont {
+        return UIFont(name: "Roboto-\(family)", size: size)!
+    }
+}
