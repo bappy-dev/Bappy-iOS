40263e4596a7f3b100d4138807d560df9702e6ff
Dongcheon Jeong <oluye7@naver.com>
2022-05-29 21:30:21 +0900

    .gitignore 생성

--- Bappy/AppDelegate/AppDelegate.swift
+++ Bappy/AppDelegate/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -27,7 +26,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
         return .portrait
     }
-
-
 }
 
