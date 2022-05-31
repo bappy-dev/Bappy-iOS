57fc52f03ffb93cf4cadaaf8697127b4f2778a68
Dongcheon Jeong <oluye7@naver.com>
2022-05-31 20:33:58 +0900

    행아웃 인원수뷰 UI

--- Bappy/Presentation/Hangout/Make/SubViews/ParticipantsLimit/HangoutParticipantsLimitView.swift
+++ Bappy/Presentation/Hangout/Make/SubViews/ParticipantsLimit/HangoutParticipantsLimitView.swift
@@ -11,6 +11,8 @@ import SnapKit
 final class HangoutParticipantsLimitView: UIView {
     
     // MARK: Properties
+    private var number: Int = 4
+    
     private let limitCaptionLabel: UILabel = {
         let label = UILabel()
         label.text = "How many people do you want to meet?"
@@ -27,6 +29,41 @@ final class HangoutParticipantsLimitView: UIView {
         return label
     }()
     
+    private let numberLabel: UILabel = {
+        let label = UILabel()
+        label.text = "4"
+        label.font = .roboto(size: 38.0, family: .Bold)
+        label.textColor = UIColor(named: "bappy_brown")
+        return label
+    }()
+    
+    private lazy var minusButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(minusButtonHandler), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var plusButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(plusButtonHandler), for: .touchUpInside)
+        return button
+    }()
+    
+    private let discriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Participants Limit  4 ~ 10"
+        label.font = .roboto(size: 10.0, family: .Light)
+        label.textColor = .black.withAlphaComponent(0.33)
+        return label
+    }()
+    
+    private let discriptionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "make_participants")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     // MARK: Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -39,9 +76,36 @@ final class HangoutParticipantsLimitView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: Actions
+    @objc
+    private func minusButtonHandler() {
+        number -= 1
+        numberLabel.text = "\(number)"
+        updateButtonState()
+    }
+    
+    @objc
+    private func plusButtonHandler() {
+        number += 1
+        numberLabel.text = "\(number)"
+        updateButtonState()
+    }
+    
     // MARK: Helpers
+    private func updateButtonState() {
+        let minusButtonImage = (number == 4) ? UIImage(named: "make_minus_off") : UIImage(named: "make_minus_on")
+        minusButton.setImage(minusButtonImage, for: .normal)
+        minusButton.isEnabled = !(number == 4)
+        
+        let plusButtonImage = (number == 10) ? UIImage(named: "make_plus_off") : UIImage(named: "make_plus_on")
+        plusButton.setImage(plusButtonImage, for: .normal)
+        plusButton.isEnabled = !(number == 10)
+        
+    }
+    
     private func configure() {
         self.backgroundColor = .white
+        updateButtonState()
     }
     
     private func layout() {
@@ -57,6 +121,40 @@ final class HangoutParticipantsLimitView: UIView {
             $0.top.equalToSuperview().inset(39.0)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(30.0)
+        }
+        
+        self.addSubview(numberLabel)
+        numberLabel.snp.makeConstraints {
+            $0.top.equalTo(hStackView.snp.bottom).offset(36.0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        self.addSubview(minusButton)
+        minusButton.snp.makeConstraints {
+            $0.width.height.equalTo(44.0)
+            $0.centerY.equalTo(numberLabel)
+            $0.centerX.equalToSuperview().offset(-70.0)
+        }
+        
+        self.addSubview(plusButton)
+        plusButton.snp.makeConstraints {
+            $0.width.height.equalTo(44.0)
+            $0.centerY.equalTo(numberLabel)
+            $0.centerX.equalToSuperview().offset(70.0)
+        }
+        
+        self.addSubview(discriptionLabel)
+        discriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(numberLabel.snp.bottom).offset(35.0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        self.addSubview(discriptionImageView)
+        discriptionImageView.snp.makeConstraints {
+            $0.centerY.equalTo(discriptionLabel)
+            $0.trailing.equalTo(discriptionLabel.snp.leading).offset(-5.0)
+            $0.width.equalTo(10.0)
+            $0.height.equalTo(8.5)
         }
     }
 }
