c56b1805ec36be0b29760eae81faf1c9071739c2
Dongcheon Jeong <oluye7@naver.com>
2022-05-31 20:04:48 +0900

    행아웃 오픈챗뷰 UI

--- Bappy/Presentation/Hangout/Make/SubViews/Openchat/HangoutOpenchatView.swift
+++ Bappy/Presentation/Hangout/Make/SubViews/Openchat/HangoutOpenchatView.swift
@@ -37,6 +37,37 @@ final class HangoutOpenchatView: UIView {
         return label
     }()
     
+    private let openchatTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .roboto(size: 14.0)
+        textField.textColor = UIColor(named: "bappy_brown")
+        textField.attributedPlaceholder = NSAttributedString(
+            string: "Enter the URL",
+            attributes: [.foregroundColor: UIColor(named: "bappy_gray")!])
+        return textField
+    }()
+    
+    private let underlinedView: UIView = {
+        let underlinedView = UIView()
+        underlinedView.backgroundColor = UIColor(red: 241.0/255.0, green: 209.0/255.0, blue: 83.0/255.0, alpha: 1.0)
+        underlinedView.addBappyShadow(shadowOffsetHeight: 1.0)
+        return underlinedView
+    }()
+    
+    private let guideButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setAttributedTitle(
+            NSAttributedString(
+                string: "Openchat guide",
+                attributes: [
+                    .foregroundColor: UIColor.black.withAlphaComponent(0.33),
+                    .font: UIFont.roboto(size: 10.0),
+                    .underlineStyle: NSUnderlineStyle.single.rawValue
+                ]),
+            for: .normal)
+        return button
+    }()
+    
     // MARK: Lifecycle
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -73,6 +104,25 @@ final class HangoutOpenchatView: UIView {
             $0.top.equalTo(openchatFirstCaptionLabel.snp.bottom).offset(2.0)
             $0.centerX.equalToSuperview()
             $0.height.equalTo(30.0)
+        }
+        
+        self.addSubview(openchatTextField)
+        openchatTextField.snp.makeConstraints {
+            $0.top.equalTo(hStackView.snp.bottom).offset(40.0)
+            $0.leading.trailing.equalToSuperview().inset(47.0)
+        }
+        
+        self.addSubview(underlinedView)
+        underlinedView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(44.0)
+            $0.height.equalTo(2.0)
+            $0.top.equalTo(openchatTextField.snp.bottom).offset(7.0)
+        }
+        
+        self.addSubview(guideButton)
+        guideButton.snp.makeConstraints {
+            $0.top.equalTo(underlinedView.snp.bottom).offset(4.0)
+            $0.trailing.equalTo(underlinedView).offset(-3.0)
         }
     }
 }
