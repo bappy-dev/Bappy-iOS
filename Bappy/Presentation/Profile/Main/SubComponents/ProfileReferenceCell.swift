9a970e67a8889c068140509019cdfea89b3365dc
cyj893 <cyj89317@gmail.com>
2022-10-19 23:39:02 +0900

    feat: Apply design
    디자인 명세에 따라 필요한 UI를 추가합니다.

--- Bappy/Presentation/Profile/Main/SubComponents/ProfileReferenceCell.swift
+++ Bappy/Presentation/Profile/Main/SubComponents/ProfileReferenceCell.swift
@@ -12,10 +12,42 @@ import Kingfisher
 final class ProfileReferenceCell: UITableViewCell {
     
     // MARK: Properties
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 25.0
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .roboto(size: 15.0, family: .Regular)
+        label.textColor = .bappyBrown
+        return label
+    }()
+    
     private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = .roboto(size: 22.0, family: .Bold)
-        label.textColor = .bappyBrown
+        label.font = .roboto(size: 15.0, family: .Medium)
+        label.textColor = .bappyYellow
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    
+    private let contentLabel: UILabel = {
+        let label = UILabel()
+        label.font = .roboto(size: 15.0, family: .Regular)
+        label.textColor = .bappyGray
+        label.numberOfLines = 3
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .roboto(size: 10.0, family: .Regular)
+        label.textColor = .bappyGray
         label.lineBreakMode = .byTruncatingTail
         return label
     }()
@@ -51,11 +83,37 @@ final class ProfileReferenceCell: UITableViewCell {
             $0.trailing.equalToSuperview().inset(13.0)
         }
         
+        frameView.addSubview(profileImageView)
+        profileImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(15.0)
+            $0.leading.equalToSuperview().inset(10.0)
+            $0.width.height.equalTo(50.0)
+        }
+        
+        frameView.addSubview(nameLabel)
+        nameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(19.0)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(7.0)
+        }
+        
         frameView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(12.0)
-            $0.leading.equalToSuperview().inset(6.0)
-            $0.trailing.lessThanOrEqualToSuperview().inset(9.0)
+            $0.top.equalTo(nameLabel.snp.bottom).offset(5.0)
+            $0.leading.equalTo(nameLabel)
+            $0.trailing.lessThanOrEqualToSuperview().inset(10.0)
+        }
+        
+        frameView.addSubview(contentLabel)
+        contentLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10.0)
+            $0.leading.equalTo(nameLabel)
+            $0.trailing.lessThanOrEqualToSuperview().inset(10.0)
+        }
+        
+        frameView.addSubview(dateLabel)
+        dateLabel.snp.makeConstraints {
+            $0.centerY.equalTo(nameLabel)
+            $0.trailing.equalToSuperview().inset(21.0)
         }
         
     }
@@ -64,6 +122,10 @@ final class ProfileReferenceCell: UITableViewCell {
 // MARK: - Bind
 extension ProfileReferenceCell {
     func bind(with reference: Reference) {
-        titleLabel.text = reference.contents
+        profileImageView.kf.setImage(with: URL(string: ""), placeholder: UIImage(named: "no_profile_l"))
+        nameLabel.text = "Good"
+        titleLabel.text = "Want to meet again"
+        dateLabel.text = "2022-22-22"
+        contentLabel.text = reference.contents
     }
 }
