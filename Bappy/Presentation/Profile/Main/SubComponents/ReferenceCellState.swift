5a440c8d941d00a17400ac18e50f606afcf1e472
cyj893 <cyj89317@gmail.com>
2022-10-20 13:32:01 +0900

    feat: Add `ReferenceCellState`
    셀을 터치하면 줄었던 것이 늘어나고, 다시 터치하면 줄어들어야 하므로 상태를 추가합니다.

---
+++ Bappy/Presentation/Profile/Main/SubComponents/ReferenceCellState.swift
@@ -0,0 +1,13 @@
+//
+//  ReferenceCellState.swift
+//  Bappy
+//
+//  Created by Yujin Cha on 2022/10/20.
+//
+
+import Foundation
+
+struct ReferenceCellState {
+    var reference: Reference
+    var isExpanded: Bool
+}
