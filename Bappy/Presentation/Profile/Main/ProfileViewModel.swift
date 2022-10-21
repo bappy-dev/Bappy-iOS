e735bde63a2b4119b20e7dc3319e7bc375c9d8ca
cyj893 <cyj89317@gmail.com>
2022-10-21 23:06:58 +0900

    feat: Add sample

--- Bappy/Presentation/Profile/Main/ProfileViewModel.swift
+++ Bappy/Presentation/Profile/Main/ProfileViewModel.swift
@@ -330,8 +330,8 @@ final class ProfileViewModel: ViewModelType {
             .flatMap(dependency.hangoutRepository.fetchHangouts)
             .map({ result in
                 return Result<[Reference], Error>(catching: {
-                    return [Reference(writerName: "Writer1", writerProfileImageURL: nil, tags: ["Nice", "Good"], contents: "내용", date: "2022-22-22", isCanRead: false, hangoutID: "1234"),
-                            Reference(writerName: "Writer2", writerProfileImageURL: nil, tags: ["Nice", "Good", "Nice", "Good", "Nice", "Good"], contents: "내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용", date: "2022-22-22", isCanRead: false, hangoutID: "1234"),
+                    return [Reference(writerName: "Writer1", writerProfileImageURL: nil, tags: ["Nice", "Good"], contents: "내용", date: "2022-22-22", isCanRead: false, hangoutID: "6350de97891e8375326533a6"),
+                            Reference(writerName: "Writer2", writerProfileImageURL: nil, tags: ["Nice", "Good", "Nice", "Good", "Nice", "Good"], contents: "내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용", date: "2022-22-22", isCanRead: false, hangoutID: "6350df0d891e8375326533a7"),
                             Reference(writerName: "Writer3", writerProfileImageURL: nil, tags: ["Nice", "Good"], contents: "내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용", date: "2022-22-22", isCanRead: true, hangoutID: "1234"),
                             Reference(writerName: "Writer4", writerProfileImageURL: nil, tags: ["Nice", "Good", "Nice", "Good", "Nice", "Good"], contents: "내용", date: "2022-22-22", isCanRead: true, hangoutID: "1234"),
                             Reference(writerName: "Writer5", writerProfileImageURL: nil, tags: ["Nice", "Good"], contents: "내용", date: "2022-22-22", isCanRead: true, hangoutID: "1234"),]
