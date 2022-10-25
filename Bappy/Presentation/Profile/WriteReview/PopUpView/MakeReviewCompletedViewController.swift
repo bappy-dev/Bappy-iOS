a175dcd418137b8b7c8c921bb514d2b66ef5d75e
cyj893 <cyj89317@gmail.com>
2022-10-25 23:18:41 +0900

    feat: Add `MakeReviewCompletedViewController` and VM
    리뷰 작성이 성공적일 경우 보여주는 팝업뷰를 추가합니다.
    later를 누르면 팝업뷰를 그냥 없애고, Okay를 누르면 프로필 화면으로 이동합니다.

---
+++ Bappy/Presentation/Profile/WriteReview/PopUpView/MakeReviewCompletedViewController.swift
@@ -0,0 +1,248 @@
+//
+//  MakeReviewCompletedViewController.swift
+//  Bappy
+//
+//  Created by Yujin Cha on 2022/10/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+import Lottie
+
+final class MakeReviewCompletedViewController: UIViewController {
+    
+    // MARK: Properties
+    private let viewModel: MakeReviewCompletedViewModel
+    private let disposeBag = DisposeBag()
+    
+    private let maxDimmedAlpha: CGFloat = 0.3
+    
+    private lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 20.0
+        view.isHidden = true
+        return view
+    }()
+    
+    private let dimmedView = UIView()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Reviews are made!"
+        label.textColor = .bappyBrown
+        label.font = .roboto(size: 28.0, family: .Bold)
+        label.adjustsFontSizeToFitWidth = true
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let bappyImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bappy_excited")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let captionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "You made BAPPY a better community!\nDo you wanna check the reviews you got?"
+        label.numberOfLines = 0
+        label.textColor = .bappyBrown
+        label.font = .roboto(size: 16.0)
+        return label
+    }()
+    
+    private let okButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .bappyYellow
+        button.setBappyTitle(
+            title: "Okay!",
+            font: .roboto(size: 24.0, family: .Medium))
+        button.layer.cornerRadius = 21.0
+        return button
+    }()
+    
+    private let forwardImageView: UIImageView = {
+        let imageView = UIImageView()
+        let configuration = UIImage.SymbolConfiguration(pointSize: 14.0, weight: .medium)
+        let image = UIImage(systemName: "chevron.forward", withConfiguration: configuration)
+        imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .bappyBrown
+        return imageView
+    }()
+    
+    private let laterButton: UIButton = {
+        let button = UIButton(type: .system)
+        let configuration = UIImage.SymbolConfiguration(pointSize: 12.0, weight: .regular)
+        let image = UIImage(systemName: "chevron.forward")
+        button.setImage(image, for: .normal)
+        button.setPreferredSymbolConfiguration(configuration, forImageIn: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.setBappyTitle(
+            title: "Maybe later ",
+            font: .roboto(size: 13.0, family: .Light),
+            hasUnderline: true)
+        return button
+    }()
+    
+    private let animationView: AnimationView = {
+        let animationView = AnimationView(name: "success")
+        animationView.contentMode = .scaleAspectFit
+        return animationView
+    }()
+    
+    // MARK: Lifecycle
+    init(viewModel: MakeReviewCompletedViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+        
+        configure()
+        layout()
+        bind()
+        playAnimation()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animateShowDimmedView()
+        animatePresentContainer()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        configureShadow()
+    }
+    
+    // MARK: Animations
+    private func playAnimation() {
+        animationView.play()
+    }
+    
+    private func animateShowDimmedView() {
+        dimmedView.backgroundColor = .black
+        dimmedView.alpha = 0
+        UIView.animate(withDuration: 0.4) {
+            self.dimmedView.alpha = self.maxDimmedAlpha
+        }
+    }
+    
+    private func animatePresentContainer() {
+        UIView.animate(withDuration: 0.4, delay: 0.4, options: .transitionCrossDissolve) {
+            self.containerView.isHidden = false
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    private func animateDismissView() {
+        UIView.animate(withDuration: 0.3) {
+            self.containerView.isHidden = true
+            self.view.layoutIfNeeded()
+        }
+        
+        dimmedView.alpha = maxDimmedAlpha
+        UIView.animate(withDuration: 0.3) {
+            self.dimmedView.alpha = 0
+        } completion: { _ in
+            self.dismiss(animated: false)
+        }
+    }
+    
+    // MARK: Helpers
+    private func configureShadow() {
+        containerView.addBappyShadow(shadowOffsetHeight: 2.0)
+    }
+    
+    private func configure() {
+        view.backgroundColor = .clear
+    }
+    
+    private func layout() {
+        view.addSubviews([dimmedView, containerView])
+        containerView.addSubviews([titleLabel, bappyImageView, captionLabel, okButton, laterButton, animationView])
+        okButton.addSubview(forwardImageView)
+        
+        dimmedView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        containerView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(27.0)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(46.0)
+            $0.leading.trailing.equalToSuperview().inset(21.0)
+        }
+        
+        bappyImageView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(13.0)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(164.0)
+            $0.height.equalTo(164.0)
+        }
+        
+        captionLabel.snp.makeConstraints {
+            $0.top.equalTo(bappyImageView.snp.bottom).offset(30.0)
+            $0.centerX.equalToSuperview()
+        }
+        
+        okButton.snp.makeConstraints {
+            $0.top.equalTo(captionLabel.snp.bottom).offset(14.0)
+            $0.leading.equalToSuperview().inset(44.0)
+            $0.trailing.equalToSuperview().inset(43.0)
+            $0.height.equalTo(42.0)
+        }
+        
+        forwardImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(18.0)
+        }
+        
+        laterButton.snp.makeConstraints {
+            $0.top.equalTo(okButton.snp.bottom)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(44.0)
+            $0.bottom.equalToSuperview().inset(5.0)
+        }
+        
+        animationView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalTo(containerView.snp.top)
+            $0.width.height.equalTo(80.0)
+        }
+    }
+}
+
+// MARK: - Bind
+extension MakeReviewCompletedViewController {
+    private func bind() {
+        okButton.rx.tap
+            .bind(to: viewModel.input.okayButtonTapped)
+            .disposed(by: disposeBag)
+        
+        laterButton.rx.tap
+            .subscribe(onNext: { [unowned self] _ in
+                self.animateDismissView()
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.output.moveToProfileView
+            .compactMap { $0 }
+            .emit(onNext: { tabBarViewModel in
+                guard let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate else { return }
+                sceneDelegate.switchRootViewToMainView(viewModel: tabBarViewModel)
+            })
+            .disposed(by: disposeBag)
+    }
+}
---
+++ Bappy/Presentation/Profile/WriteReview/PopUpView/MakeReviewCompletedViewModel.swift
@@ -0,0 +1,68 @@
+//
+//  MakeReviewCompletedViewModel.swift
+//  Bappy
+//
+//  Created by Yujin Cha on 2022/10/25.
+//
+
+import Foundation
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+final class MakeReviewCompletedViewModel: ViewModelType {
+    struct Dependency {
+        let user: BappyUser
+        
+        init(user: BappyUser) {
+            self.user = user
+        }
+    }
+    
+    struct Input {
+        var okayButtonTapped: AnyObserver<Void>
+    }
+    
+    struct Output {
+        var moveToProfileView: Signal<BappyTabBarViewModel?>
+    }
+    
+    let dependency: Dependency
+    var disposeBag = DisposeBag()
+    let input: Input
+    let output: Output
+    
+    private let okayButtonTapped$ = PublishSubject<Void>()
+    
+    private let moveToProfileView$ = PublishSubject<BappyTabBarViewModel?>()
+    
+    init(dependency: Dependency) {
+        self.dependency = dependency
+        
+        // MARK: Streams
+        let moveToEditProfileView = moveToProfileView$
+            .asSignal(onErrorJustReturn: nil)
+        
+        // MARK: Input & Output
+        self.input = Input(
+            okayButtonTapped: okayButtonTapped$.asObserver()
+        )
+        
+        self.output = Output(
+            moveToProfileView: moveToEditProfileView
+        )
+        
+        // MARK: Binding
+        okayButtonTapped$
+            .map { _  -> BappyTabBarViewModel in
+                let tabBarDependency = BappyTabBarViewModel.Dependency(
+                    selectedIndex: 1,
+                    user: dependency.user)
+                let tabBarViewModel = BappyTabBarViewModel(dependency: tabBarDependency)
+                return BappyTabBarViewModel(dependency: tabBarDependency)
+            }
+            .bind(to: moveToProfileView$)
+            .disposed(by: disposeBag)
+    }
+}
