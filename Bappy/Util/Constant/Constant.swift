1011c90caca97a37fa896997c6cc0d586f5590c9
이현욱 <hjhsha827@gmail.com>
2022-12-16 19:53:25 +0900

    인스타 메이킹 가이드 URL 변경

--- Bappy/Util/Constant/Constant.swift
+++ Bappy/Util/Constant/Constant.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 struct Constant {
-    static let instaURL = "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTE3NDU5OTgyNTc0MDEx?story_media_id=2907455711659592766_51659214498&igshid=YmMyMTA2M2Y="
+    static let instaURL =
+    "https://www.instagram.com/s/aGlnaGxpZ2h0OjE3OTE3NDU5OTgyNTc0MDEx?story_media_id=2907455711659592766&igshid=YmMyMTA2M2Y="
     
     static let hangoutDefaultImages = [UIImage(named: "hangout_default_1"), UIImage(named: "hangout_default_2"), UIImage(named: "hangout_default_3"), UIImage(named: "hangout_default_4"), UIImage(named: "hangout_default_5")]
 }
